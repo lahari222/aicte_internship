1a35fcb44a3425513ffab086d545f0312c63391e
lahari222 <98799870+lahari222@users.noreply.github.com>
2022-09-20 15:01:45 +0530

    Update my file1.c

--- day1/my file1.c
+++ day1/my file1.c
@@ -1,1 +1,14 @@
-
+program to find size of the primary datatypes.
+  #include<stdio.h>
+  int main(){
+  int intType;
+  float floatType;
+  double doubleType;
+  char charTypes;
+  printf("%d",sizeof(intType));
+  printf("%f",sizeof(floatType));
+  printf("%lf",sizeof(doubleType));
+  printf("%c",sizeof(charType));
+  return 0;
+}
+practise the sample programs to get familiar with compiling and ececution of c program in linux.
